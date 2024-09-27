5c87096de2f9153b67528516e973bf6f988b49f3
Edwin Castro <edwincastro491@gmail.com>
2024-09-27 06:48:19 -0500

    Primer commit

---
+++ resources/js/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCPKrj9Ti6C8hNHzRKJvgf2xrcQoFI5RQU",
+  authDomain: "mascotas-31be5.firebaseapp.com",
+  projectId: "mascotas-31be5",
+  storageBucket: "mascotas-31be5.appspot.com",
+  messagingSenderId: "301701445833",
+  appId: "1:301701445833:web:8dabaae0c0941ff6b7f518"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
